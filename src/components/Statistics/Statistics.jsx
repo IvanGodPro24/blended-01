b7033a0581ba1cd622a5317bb0214fe053b8837f
Teslya Stepan <stepan199812@gmail.com>
2024-12-15 12:06:56 +0200

    task 2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,30 @@
-// import { FaRegThumbsUp } from 'react-icons/fa';
-// import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
-// import { GiTreeDoor } from 'react-icons/gi';
-// import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
-// import style from './Statistics.module.css';
+import { FaRegThumbsUp } from 'react-icons/fa';
+import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
+import { GiTreeDoor } from 'react-icons/gi';
+import StatisticsItem from '../StatisticsItem/StatisticsItem';
+import style from './Statistics.module.css';
 
-const Statistics = () => {
-  return <h2>Statistics</h2>;
+const Statistics = ({ title, stats }) => {
+  const icons = [
+    <FaRegThumbsUp />,
+    <MdPeople />,
+    <MdOutlineProductionQuantityLimits />,
+    <GiTreeDoor />,
+  ];
+  return (
+    <div>
+      {title && <h3 className={style.title}>Main Statistics</h3>}
+      <ul className={style.list}>
+        {stats.map(({ id, total, title }, index) => {
+          return (
+            <li className={style.item} key={id}>
+              <StatisticsItem total={total} title={title} icon={icons[index]} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Statistics;
